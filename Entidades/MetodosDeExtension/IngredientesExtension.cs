7fcc51c7c67eaf4b343dcf4c5103ea0bd84b63a6
Agustin Mondani <a.mondani@hotmail.com>
2024-07-26 09:18:37 -0300

    inteface y extensiones

--- Entidades/MetodosDeExtension/IngredientesExtension.cs
+++ Entidades/MetodosDeExtension/IngredientesExtension.cs
@@ -3,9 +3,35 @@
 
 namespace Entidades.MetodosDeExtension
 {
-    public class IngredientesExtension
+    public static class IngredientesExtension
     {
+        public static double CalcularCostoIngredientes(this List<EIngrediente> ingredientes, int costoInicial)
+        {
+            double costoFinal = costoInicial;
+            foreach(EIngrediente i in ingredientes) 
+            {
+                costoFinal += (int)i;
+            }
 
+            return costoFinal;
+        }
+
+        public static List<EIngrediente> IngredientesAleatorios(this Random rand)
+        {
+            int numeroRandom = rand.Next(1, 6);
+
+
+            List<EIngrediente> ingredientes = new List<EIngrediente>()
+            {
+                EIngrediente.QUESO,
+                EIngrediente.PANCETA,
+                EIngrediente.ADHERESO,
+                EIngrediente.HUEVO,
+                EIngrediente.JAMON,
+            };
+
+            return ingredientes.Take(numeroRandom).ToList();
+        }
 
     }
 }
